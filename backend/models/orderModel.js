ad5f7a65affa0afcb157fc704ccb48f5d6734239
Adnaan <abuuzeyd@gmail.com>
2021-06-08 19:15:21 +0200

    orderModel implemented

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,34 +1,78 @@
 import mongoose from 'mongoose'
 
-const orderSchema = mongoose.Schema ({
-        user: {
-            
-            type: mongoose.Schema.Types.ObjectId,
-            require: true,
-            ref:'User'
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
         },
-        orderItems:[ {
-            
-            name: {type:String, require:true},
-            qtw: {type:Number, require:true},
-            image: {type:String, require:true},
-        }],
-        password: {
-            
-            type: String,
-            require: true
-        },
-        isadmin: {
-            
-            type: Boolean,
-            require: true,
-            default: false
-        }
-}, 
-{
-    timestamps: true
-})
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const User = mongoose.model('User', userSchema)
+const Order = mongoose.model('Order', orderSchema)
 
-export default User
+export default Order
